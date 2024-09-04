6fe51708672118c925ff5d5aeaec65ba60ae7943
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-04 15:56:44 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let age = 10;
+console.log(age);
+
+let score = 95;
+console.log(score);
+
+const pi = 3.14159;
+console.log(pi);
+
+let isStudent = false;
+console.log(isStudent);
+
+let colors = ['red', 'green', 'yellow'];
+console.log(colors);
